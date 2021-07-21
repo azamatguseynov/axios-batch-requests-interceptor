b2d548308a07879353d70feb1572ebf5a5e95139
azamatguseynov <guseynov@adv.ru>
2021-07-21 20:00:11 +0300

    - refactoring

--- src/axios/batchInterceptor/adapter.ts
+++ src/axios/batchInterceptor/adapter.ts
@@ -1,4 +1,4 @@
-import {AxiosAdapter} from "axios";
+import {AxiosAdapter, AxiosRequestConfig} from "axios";
 import {UUID, uuidv4} from "./helpers/uuid";
 import {createDelay} from "./helpers/delay";
 import {BatchInterceptorConfig} from "./interceptor";
@@ -12,33 +12,37 @@ export type BatchAdapterConfig<T> = {
 const DEFAULT_DELAY = 0;
 
 export const batchRequestAdapter = <T>(adapterConfig: BatchAdapterConfig<T>): AxiosAdapter => {
-    const { delayMs = DEFAULT_DELAY, rejectFn } = adapterConfig;
     let butchRequestConfigs: RequestConfigRecord = {};
     let butchRequestPromises: Record<UUID, BatchRequestPromise<T>> = {};
+    const { delayMs = DEFAULT_DELAY, rejectFn } = adapterConfig;
+
     const delay = createDelay(delayMs);
+
+    const clearBatchRequestConfigs = () => butchRequestConfigs = {};
+    const saveBatchRequestConfig = (requestUuid: UUID, config: AxiosRequestConfig) => butchRequestConfigs[requestUuid] = config;
+    const clearBatchRequestByUuid = (batchRequestUuid: UUID) => () => delete butchRequestPromises[batchRequestUuid];
+    const getCurrentResponseByUuid = (requestUuid: UUID) => <T>(butchRequestResponse: BatchRequestResponse<T>) => {
+        return butchRequestResponse[requestUuid];
+    }
 
     return async (config) => {
         const requestUuid = uuidv4();
 
-        butchRequestConfigs[requestUuid] = config;
+        saveBatchRequestConfig(requestUuid, config);
 
         // will return the same uuid for all requests until timout is triggered
         const batchRequestUuid = await delay();
 
-        const onSuccess = <T>(butchRequestResponse: BatchRequestResponse<T>) => {
-            delete butchRequestPromises[batchRequestUuid];
-
-            return butchRequestResponse[requestUuid];
-        }
-
         if (!butchRequestPromises[batchRequestUuid]) {
             butchRequestPromises[batchRequestUuid] = batchRequest(butchRequestConfigs, adapterConfig);
-            butchRequestConfigs = {};
+
+            clearBatchRequestConfigs();
         }
 
         return butchRequestPromises[batchRequestUuid]
-            .then(onSuccess)
-            .then(rejectFn);
+            .then(getCurrentResponseByUuid(requestUuid))
+            .then(rejectFn)
+            .finally(clearBatchRequestByUuid(batchRequestUuid));
     }
 }
 
